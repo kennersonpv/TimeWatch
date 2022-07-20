b8f1e9eb0d94b5c7a8721b53a8f14210f1c87f67
kennersonpv <kennersondudu@hotmail.com>
2022-07-20 19:01:14 -0300

    Add project files.

---
+++ TimeWatch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace TimeWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Watch.StartWatch();
+                Thread.Sleep(3000);
+
+                Console.WriteLine(Watch.StopWatch());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ TimeWatch/Watch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeWatch
+{
+    public static class Watch
+    {
+        private static DateTime _startTime;
+        private static DateTime _stopTime;
+        private static bool isRunnig = false;
+
+        public static void StartWatch()
+        {
+            if (isRunnig)
+            {
+                throw new InvalidOperationException();
+            }
+
+            _startTime = DateTime.Now;
+            isRunnig = true;
+        }
+
+        public static TimeSpan StopWatch()
+        {
+            if (!isRunnig)
+            {
+                throw new InvalidOperationException();
+            }
+
+            _stopTime = DateTime.Now;
+            return _stopTime - _startTime;
+        }
+
+    }
+}
